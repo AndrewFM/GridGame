b0f5e7c14d60eaa3b8c721329dedfdd2f2ab0c00
AndrewFM <AndrewFMs@gmail.com>
2015-10-26 19:32:20 -0400

    Cleaning up ggmap.py

--- ggmap.py
+++ ggmap.py
@@ -1,5 +1,6 @@
 # Data structure for the playing grid that the game takes place on.
 import pygame
+
 class MapGrid():
 
 	def __init__(self, width, height):
@@ -8,14 +9,11 @@ class MapGrid():
 		self.HEIGHT = height
 		self.MARGIN = 5
 
-		grid = [[0 for row in range(10)] for column in range(10)]
+		self.grid = [[0 for row in range(10)] for column in range(10)]
 
 	def update(self, event):
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			pos = pygame.mouse.get_pos()
-			column = pos[0] / (self.WIDTH + self.MARGIN)
-			row = pos[1] / (self.HEIGHT + self.MARGIN)
-			grid[row][column] = 1
+		#TODO handling of keyboard input, mouse clicks, etc for the map scene
+		todo = 0
 
 	# Renders the map on screen
 	def renderGrid(self, screen):
